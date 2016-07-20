107eddf94e0a8ea2ea6c99d3ff4e56033cc56813
Srividya Ramakrishnan <srividya.ramki@gmail.com>
2016-07-19 22:55:23 -0400

    My first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name travelappv2App
+ * @description
+ * # travelappv2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('travelappv2App', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
